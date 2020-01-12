ae928bdbdaf473623be0d27d647cb6a91fb03620
cute-dd <48754862+cute-dd@users.noreply.github.com>
2020-01-12 22:44:57 +0800

    2020.1.12

---
+++ Assets/Scripts/FinalMoveMent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalMoveMent : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    private Animator anim;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
