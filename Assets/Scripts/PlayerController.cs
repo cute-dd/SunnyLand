1d5761c5daaa665659f31d9e2d5fa7ea0ed1f878
cute-dd <48754862+cute-dd@users.noreply.github.com>
2020-01-15 17:06:59 +0800

    2020.1.15

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim; 
+    private Rigidbody2D rb;
+    private Animator anim;
+    public Collider2D coll;
+    public LayerMask ground;
     public float speed;
     public float jumpForce;
+    public int cherry, gem;
     
     
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         Movement();
+        SwitchAnimation();
     }
 
+    /// <summary>
+    /// 移动
+    /// </summary>
     void Movement()
     {
         float horizontalMove = Input.GetAxis("Horizontal");
@@ -45,8 +55,36 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 动作切换
+    /// </summary>
     void SwitchAnimation()
     {
-         
+        if (anim.GetBool("Jumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("Jumping", false);
+                anim.SetBool("Falling", true);
+            }
+        }else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("Falling", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Cherry"))
+        {
+            Destroy(other.gameObject);
+            cherry += 1;
+        }
+
+        if (other.CompareTag("Gem"))
+        {
+            Destroy(other.gameObject);
+            gem += 1;
+        }
     }
 }
